2ff49b18774310d003076c3daa016bbff690f873
wtaype <wilder.taype16@gmail.com>
2025-05-17 23:19:10 -0500

    El mejor proyecto zitime

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css'
+import { setupCounter } from './widev.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="https://i.postimg.cc/bvWtMmR7/image.png" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Hola Wilder nuevo!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Buenos dias amigo
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+ 
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/zitime/', // Esta es la dirección para la versión más nueva
+})
